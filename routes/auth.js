a6486c253456e46fd2dae05979b7cf7ff2e42c02
LipingSun <liping.sun@sjsu.edu>
2015-07-27 23:59:32 -0700

    complete auth module

---
+++ routes/auth.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var User = require('./../models/User');
+
+var auth = {};
+
+auth.getToken = function (req, res) {
+
+};
+
+auth.login = function (req, res) {
+    res.json('login success');
+};
+
+auth.register = function (req, res) {
+    User.create(req.body, function (err, data) {
+        if (err) {
+            res.status(500).send();
+        } else {
+            req.login({email: req.body.email, password: req.body.password}, function(err, next) {
+                if (err) {
+                    return next(err);
+                } else {
+                    return res.status(201).json("insert id: " + data.insertId);
+                }
+            });
+        }
+    });
+};
+
+
+auth.logout = function (req, res) {
+    req.logout();
+    res.json('logout success');
+};
+
+module.exports = auth;
