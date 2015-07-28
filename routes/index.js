dd1e2785b7d94f5a7e8516ca58d27717016422a0
LipingSun <liping.sun@sjsu.edu>
2015-07-28 00:06:06 -0700

    complete index module

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,16 @@
 
 var routes = {};
 
-/* GET home page. */
 routes.index = function (req, res) {
     res.render('index.html');
 };
 
 routes.login = function (req, res) {
-
+    res.render('login.html');
 };
 
 routes.register = function (req, res) {
-
+    res.render('register.html');
 };
 
 module.exports = routes;
