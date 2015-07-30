8dcc0f1c7ab8daccf0b7512460978847e66cf503
LipingSun <liping.sun@sjsu.edu>
2015-07-29 18:32:23 -0700

    complete emulator module

--- routes/emulators.js
+++ routes/emulators.js
@@ -1,25 +1,55 @@
 'use strict';
 
+var Emulator = require('./../models/Emulator');
+
 var emulators = {};
 
 emulators.getEmulators = function (req, res) {
-
-};
-
-emulators.launchEmulators = function (req, res) {
-
+    Emulator.findAll(function (err, data) {
+        if (!err) {
+            res.status(200).json(data);
+        }
+    });
 };
 
 emulators.getEmulator = function (req, res) {
+    if (req.params.id) {
+        Emulator.findById(req.params.id, function (err, data) {
+            if (!err) {
+                res.status(200).json(data);
+            }
+        });
+    } else {
+        res.status(500).json('Require id');
+    }
+};
 
+emulators.launchEmulators = function (req, res) {
+    Emulator.create(req.body, function () {
+        if (!err) {
+            res.status(201).json(data);
+        }
+    });
 };
 
 emulators.updateEmulator = function (req, res) {
-
+    Emulator.update(req.body, function () {
+        if (!err) {
+            res.status(201).json(data);
+        }
+    });
 };
 
 emulators.terminateEmulator = function (req, res) {
-
+    var changes = {
+        id: req.param.id,
+        status: 'terminated'
+    };
+    Emulator.update(changes, function () {
+        if (!err) {
+            res.status(200).json(data);
+        }
+    });
 };
 
 module.exports = emulators;
