8b7d064c24d2919e9d3fdf1c3e874533d24965bd
LipingSun <liping.sun@sjsu.edu>
2015-08-01 20:06:19 -0700

    complete emulator ctrl for $resource

--- public/js/angularjs/controllers/emulators-controller.js
+++ public/js/angularjs/controllers/emulators-controller.js
@@ -1,4 +1,29 @@
-angular.module('myApp').controller('EmulatorsController', function (emulatorsService) {
+angular.module('myApp').controller('EmulatorsController', ['$window', 'emulatorsService', function ($window, emulatorsService) {
+
     var ctrl = this;
-    ctrl.emulators = emulatorsService.query();
-});
+
+    ctrl.getAll = function () {
+        ctrl.emulators = emulatorsService.query();
+    };
+
+    ctrl.getOne = function (id) {
+        ctrl.emulator = emulatorsService.get(id);
+    };
+
+    ctrl.create = function (newEmulator) {
+        var emulator = new emulatorsService(newEmulator);
+        emulator.$save();
+    };
+
+    ctrl.delete = function (emulator) {
+        emulator.$delete({id: emulator.id});
+    };
+
+    ctrl.getAll();
+
+    ctrl.view = function (emulator) {
+        var params = 'host=' + emulator.ip + '&' +'port=' + emulator.vnc_port;
+        $window.open('bower_components/no-vnc/vnc_auto.html?' + params, emulator.name, 'height=800, width=480');
+    };
+
+}]);
