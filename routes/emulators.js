3d2423cc5f63aad04d246a785c4407f1eae6008c
LipingSun <liping.sun@sjsu.edu>
2015-08-01 20:05:21 -0700

    manipulate controller module distinguish user type for getEmulators

--- routes/emulators.js
+++ routes/emulators.js
@@ -1,21 +1,25 @@
 'use strict';
 
-var request = require('request');
-
+var controller = require('./../services/controller');
 var Emulator = require('./../models/Emulator');
 var DeviceHost = require('./../models/DeviceHost');
 
 var emulators = {};
 
-emulators.getEmulators = function (req, res) {
+emulators.getEmulators = function (req, res) {// TODO
     Emulator.findAll(function (err, data) {
         if (!err) {
+            if (req.user.type === 'user') {
+                data = data.filter(function (emulator) {
+                    return emulator.user_id === req.user.id && (emulator.status === 'running' || emulator.status === 'processing');
+                });
+            }
             res.status(200).json(data);
         }
     });
 };
 
-emulators.getEmulator = function (req, res) {
+emulators.getEmulator = function (req, res) {// TODO
     if (req.params.id) {
         Emulator.findById(req.params.id, function (err, data) {
             if (!err) {
@@ -28,27 +32,44 @@ emulators.getEmulator = function (req, res) {
 };
 
 emulators.launchEmulators = function (req, res) {
-    //DeviceHost.findAll(function (err, data) {
-    //    if (!err) {
-    //        var deviceHost = data[0];
-    //        //request.post();
-    //    }
-    //});
-
-
-    req.body.status = 'processing';
-    Emulator.create(req.body, function (err, data) {
-        if (!err) {
-            Emulator.findById(data.insertId, function (err, data) {
-                if (!err) {
-                    res.status(201).json(data);
-                }
-            });
-        }
-    });
+    for (var i = 1; i <= req.body.number; i++) {
+        var newEmulator = req.body;
+        newEmulator.status = 'processing';
+        newEmulator.user_id = req.user.id;
+        if (req.body.number > 1) newEmulator.name += '_' + i;
+        delete newEmulator.number;
+        Emulator.create(newEmulator, function (err, emulator) {
+            if (!err) {
+                DeviceHost.findOne({hostname: 'host1'} ,function (err, deviceHost) {
+                    if (!err) {
+                        var addr = 'http://' + deviceHost.ip;
+                        if (deviceHost.port) {
+                            addr += ':' + deviceHost.port;
+                        }
+                        emulator.id = Number(emulator.id);
+                        controller.launchEmulator(addr, emulator, function (err, data) {
+                            if (!err) {
+                                var changes = {
+                                    ip: deviceHost.ip,
+                                    status: 'running'
+                                };
+                                if (data.VNCPort) changes.vnc_port =  data.VNCPort;
+                                if (data.SSHPort) changes.ssh_port =  data.SSHPort;
+                                Emulator.update(emulator.id, changes, function (err, data) {
+                                    if (!err) {
+                                        res.status(201).json(data);
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
 };
 
-emulators.updateEmulator = function (req, res) {
+emulators.updateEmulator = function (req, res) {// TODO
     Emulator.update(req.params.id, req.body, function (err, data) {
         if (!err) {
             Emulator.findById(req.params.id, function (err, data) {
@@ -61,9 +82,23 @@ emulators.updateEmulator = function (req, res) {
 };
 
 emulators.terminateEmulator = function (req, res) {
-    Emulator.update(req.params.id, { status: 'terminated'}, function (err, data) {
+    DeviceHost.findOne({hostname: 'host1'} ,function (err, deviceHost) {
         if (!err) {
-            res.status(200).json(data);
+            var addr = 'http://' +  deviceHost.ip;
+            if (deviceHost.port) {
+                addr += ':' + deviceHost.port;
+            }
+            controller.terminateEmulator(addr, req.params.id, function () {
+                var changes = {
+                    status: 'terminated',
+                    terminate_datetime: new Date().toISOString()
+                };
+                Emulator.update(req.params.id, changes, function (err, data) {
+                    if (!err) {
+                        res.status(200).json(data);
+                    }
+                });
+            });
         }
     });
 };
