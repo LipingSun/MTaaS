4e6803f5405b3cc912b688f3852be8fd7fbca0ea
LipingSun <liping.sun@sjsu.edu>
2015-08-02 03:12:22 -0700

    restructure controller module

--- services/controller.js
+++ services/controller.js
@@ -4,59 +4,90 @@ var request = require('request');
 
 var controller = {};
 
-controller.getEmulators = function (addr, callback) {
-    request.get(this.addr + '/emulators', function (err, res, body) {
-        if (!err && res.statusCode == 200) {
-            callback(body);
-        } else {
-            console.log(err);
-        }
-    });
-};
+controller.emulator = {
 
-/*controller.getEmulator = function (addr) {
-    if (req.params.id) {
-        Emulator.findById(req.params.id, function (err, data) {
-            if (!err) {
-                res.status(200).json(data);
+    getAll: function (addr, callback) {
+        request.get(this.addr + '/emulators', function (err, res, body) {
+            if (!err && res.statusCode == 200) {
+                callback(body);
+            } else {
+                console.log(err);
             }
         });
-    } else {
-        res.status(500).json('Require id');
-    }
-};*/
+    },
 
-controller.launchEmulator = function (addr, emulator, callback) {
-    request.post(addr + '/emulators', { json: emulator }, function (err, res, body) {
-        if (!err && res.statusCode == 201) {
-            callback(null, body);
-        } else {
-            console.log(err);
-            callback(err);
-        }
-    });
+    getById: function () { //TODO
+
+    },
+
+    launch: function (addr, emulator, callback) {
+        request.post(addr + '/emulators', { json: emulator }, function (err, res, body) {
+            if (!err && res.statusCode == 201) {
+                callback(null, body);
+            } else {
+                console.log(err);
+                callback(err);
+            }
+        });
+    },
+
+    update: function () { //TODO
+
+    },
+
+    terminate: function (addr, id, callback) {
+        request.del(addr + '/emulators/' + Number(id), function (err, res) {
+            if (!err && res.statusCode == 200) {
+                callback();
+            } else {
+                console.log(err);
+            }
+        });
+    }
+
 };
 
-/*controller.updateEmulator = function (addr) {
-    Emulator.update(req.params.id, req.body, function (err, data) {
-        if (!err) {
-            Emulator.findById(req.params.id, function (err, data) {
-                if (!err) {
-                    res.status(201).json(data);
-                }
-            });
-        }
-    });
-};*/
+controller.hub = {
 
-controller.terminateEmulator = function (addr, id, callback) {
-    request.del(addr + '/emulators/' + Number(id), function (err, res) {
-        if (!err && res.statusCode == 200) {
-            callback();
-        } else {
-            console.log(err);
-        }
-    });
+    getAll: function (addr, callback) {
+        request.get(this.addr + '/hubs', function (err, res, body) {
+            if (!err && res.statusCode == 200) {
+                callback(body);
+            } else {
+                console.log(err);
+            }
+        });
+    },
+
+    getById: function () { //TODO
+
+    },
+
+    launch: function (addr, hub, callback) {
+        request.post(addr + '/hubs', { json: hub }, function (err, res, body) {
+            if (!err && res.statusCode == 201) {
+                callback(null, body);
+            } else {
+                console.log(err);
+                callback(err);
+            }
+        });
+    },
+
+    update: function () { //TODO
+
+    },
+
+    terminate: function (addr, id, callback) {
+        request.del(addr + '/hubs/' + Number(id), function (err, res) {
+            if (!err && res.statusCode == 200) {
+                callback();
+            } else {
+                console.log(err);
+            }
+        });
+    }
+
 };
 
 module.exports = controller;
