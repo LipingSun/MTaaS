13a7cec7d76253262c964678b88e97bf4d75f75f
LipingSun <liping.sun@sjsu.edu>
2015-08-01 19:51:51 -0700

    when create or update an emulator, return the new emulator

--- models/Emulator.js
+++ models/Emulator.js
@@ -6,34 +6,44 @@ var squel = require('squel');
 var Emulator = {};
 
 Emulator.findAll = function (callback) {
-    var sql = squel.select().from('emulator').toString();
-    mysql.query(sql, callback);
+    var sql = squel.select().from('emulator');
+    mysql.query(sql.toString(), callback);
 };
 
 Emulator.findById = function (id, callback) {
-    var sql = squel.select().from('emulator').where('id=' + id).toString();
-    mysql.queryOne(sql, callback);
+    var sql = squel.select().from('emulator').where('id = ' + id);
+    mysql.queryOne(sql.toString(), callback);
 };
 
 Emulator.findOne = function (obj, callback) {
-    var condition = Object.keys(obj)[0] + '=' +  mysql.escape(obj[Object.keys(obj)[0]]);
-    var sql = squel.select().from('emulator').where(condition).toString();
-    mysql.queryOne(sql, callback);
+    var condition = Object.keys(obj)[0] + ' = ' +  mysql.escape(obj[Object.keys(obj)[0]]);
+    var sql = squel.select().from('emulator').where(condition);
+    mysql.queryOne(sql.toString(), callback);
 };
 
 Emulator.create = function (emulator, callback) {
     var sql = squel.insert().into('emulator').setFields(emulator).toString();
-    mysql.query(sql, callback);
+    mysql.query(sql, function (err, data) {
+        if (!err) {
+            sql = squel.select().from('emulator').where('id = ' + data.insertId);
+            mysql.queryOne(sql.toString(), callback);
+        }
+    });
 };
 
 Emulator.deleteById = function (id, callback) {
-    var sql = squel.delete().from('emulator').where('id=' + id).toString();
-    mysql.query(sql, callback);
+    var sql = squel.delete().from('emulator').where('id = ' + id);
+    mysql.query(sql.toString(), callback);
 };
 
-Emulator.update = function (emulator, callback) {
-    var sql = squel.update().table('emulator').setFields(emulator).where('id=' + emulator.id);
-    mysql.query(sql, callback);
+Emulator.update = function (id, emulator, callback) {
+    var sql = squel.update().table('emulator').setFields(emulator).where('id = ' + id);
+    mysql.query(sql.toString(), function (err) {
+        if (!err) {
+            sql = squel.select().from('emulator').where('id = ' + id);
+            mysql.queryOne(sql.toString(), callback);
+        }
+    });
 };
 
 module.exports = Emulator;
