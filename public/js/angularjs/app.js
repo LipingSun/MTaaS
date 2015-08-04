2a36856c6d6dfe3ea6e44274ddde641fe3c0e36d
LipingSun <liping.sun@sjsu.edu>
2015-08-04 00:57:00 -0700

    divided

--- public/js/angularjs/app.js
+++ public/js/angularjs/app.js
@@ -4,3 +4,8 @@ angular.module('myApp', ['ngRoute', 'ngResource', 'chart.js'])
     //.constant('host', 'http://private-e5a74-mtaas.apiary-mock.com')
     .constant('host', '')
 ;
+
+angular.module('adminApp', ['ngRoute', 'ngResource', 'chart.js'])
+    //.constant('host', 'http://private-e5a74-mtaas.apiary-mock.com')
+    .constant('host', '')
+;
