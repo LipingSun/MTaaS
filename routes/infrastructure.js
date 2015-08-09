578bca35a356f1e260c63a502d718a2e93a979b5
LipingSun <liping.sun@sjsu.edu>
2015-08-09 02:32:55 -0700

    init infrastructure

---
+++ routes/infrastructure.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var controller = require('./../services/controller');
+var Emulator = require('./../models/Emulator');
+var Hub = require('./../models/Hub');
+var ControllerHost = require('./../models/ControllerHost');
+var squel = require('squel');
+
+var infrastructure = {};
+
+infrastructure.setup = function (req, res) {
+
+    // Launch hub
+    var newHub = req.body.infrastructure.hub;
+    newHub.status = 'processing';
+    newHub.user_id = req.user.id;
+    Hub.create(newHub, function (err, hub) {
+        if (!err) {
+            var host_id = '00000000001';
+            ControllerHost.findById(host_id, function (err, controllerHost) {
+                if (!err) {
+                    var host = 'http://' + controllerHost.ip;
+                    if (controllerHost.port) {
+                        host += ':' + controllerHost.port;
+                    }
+                    controller.hub.launch(host, hub, function (err, data) {
+                        if (!err) {
+                            var changes = {
+                                host_id: controllerHost.id,
+                                status: 'running'
+                            };
+                            Hub.update(hub.id, changes, function (err, data) {
+                                if (!err) {
+                                    // Launch emulators
+                                    for (var i = 1; i <= req.body.infrastructure.emulator.number; i++) {
+                                        var newEmulator = req.body.infrastructure.emulator;
+                                        newEmulator.status = 'processing';
+                                        newEmulator.user_id = req.user.id;
+                                        if (newEmulatorNum > 1) newEmulator.name += '_' + i;
+                                        delete newEmulator.number;
+                                        Emulator.create(newEmulator, function (err, emulator) {
+                                            if (!err) {
+                                                controller.emulator.launch(host, emulator, function (err, data) {
+                                                    if (!err) {
+                                                        var changes = {
+                                                            host_id: controllerHost.id,
+                                                            ip: controllerHost.ip,
+                                                            vnc_port: data.VNCPort,
+                                                            ssh_port: data.SSHPort,
+                                                            status: 'running'
+                                                        };
+                                                        Emulator.update(emulator.id, changes, function (err, data) {
+                                                            if (!err) {
+                                                                // Attach emulators to hub
+                                                                controller.hub.attach(host, hub.id, {resource_id: emulator.id, resource_type: 'emulator'}, function (err, data) {
+                                                                    if (!err) {
+                                                                        var changes = {
+                                                                            hub_id: data.hub_id,
+                                                                            hub_port: data.hub_port
+                                                                        };
+                                                                        Emulator.update(emulator.id, changes, function (err, data) {
+                                                                            if (i === req.body.infrastructure.emulator.number)
+                                                                                res.status(201);
+                                                                        });
+                                                                    }
+                                                                });
+                                                            }
+                                                        });
+                                                    } else {
+                                                        var changes = {
+                                                            status: 'error'
+                                                        };
+                                                        Emulator.update(emulator.id, changes, function (err, data) {
+                                                            if (!err) {
+                                                                res.status(500).json(data);
+                                                            }
+                                                        });
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    }
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+};
+
+module.exports = infrastructure;
