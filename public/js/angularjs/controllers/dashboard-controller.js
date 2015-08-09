98cc740fccefad62d4569228db1b678ba4db3247
LipingSun <liping.sun@sjsu.edu>
2015-08-09 02:23:03 -0700

    dashboard implementation

--- public/js/angularjs/controllers/dashboard-controller.js
+++ public/js/angularjs/controllers/dashboard-controller.js
@@ -1,4 +1,4 @@
-angular.module('myApp').controller('DashboardController', ['emulatorsService', 'hubsService', '$http', function (emulatorsService, hubsService, $http) {
+angular.module('myApp').controller('DashboardController', ['emulatorsService', 'devicesService', 'hubsService', '$http', function (emulatorsService, devicesService, hubsService, $http) {
 
     const api_v1 = '/api/v1';
 
@@ -33,102 +33,133 @@ angular.module('myApp').controller('DashboardController', ['emulatorsService', '
     var draw = function () {
 
         ctrl.emulators = emulatorsService.query(function () {
+
             ctrl.emulators_num = ctrl.emulators.length;
 
-            ctrl.hubs = hubsService.query(function () {
-                ctrl.hubs_num = ctrl.hubs.length;
+            ctrl.devices = devicesService.query(function () {
 
-                var nodes = [];
-                var edges = [];
+                ctrl.devices_num = ctrl.devices.length;
 
-                var DIR = '../../../images/resource/';
-                var EDGE_LENGTH_MAIN = 150;
-                var EDGE_LENGTH_SUB = 50;
+                ctrl.hubs = hubsService.query(function () {
+                    ctrl.hubs_num = ctrl.hubs.length;
 
-                // alert(ctrl.hubs.length+' '+ctrl.emulators.length);
+                    var nodes = [];
+                    var edges = [];
 
-                var wifi_node = 0;
+                    var DIR = '../../../images/resource/';
+                    var EDGE_LENGTH_MAIN = 150;
+                    var EDGE_LENGTH_SUB = 50;
 
-                for (var i = 0; i < ctrl.hubs.length; i++) {
+                    // alert(ctrl.hubs.length+' '+ctrl.emulators.length);
 
-                    wifi_node++;
-                    nodes.push({
-                        id: ctrl.hubs[i].id,
-                        label: 'HUB: ' + ctrl.hubs[i].name,
-                        image: DIR + 'HUB.png',
-                        shape: 'image'
-                    });
+                    var wifi_node = 0;
 
+                    for (var i = 0; i < ctrl.hubs.length; i++) {
 
-                    //alert(ctrl.hubs[i].network_type);
-                    if (ctrl.hubs[i].network_type == 'WiFi') {
+                        wifi_node++;
                         nodes.push({
-                            id: wifi_node,
-                            label: 'WIFI',
-                            image: DIR + 'WIFI.png',
+                            id: ctrl.hubs[i].id,
+                            label: 'HUB: ' + ctrl.hubs[i].name,
+                            image: DIR + 'HUB.png',
                             shape: 'image'
                         });
 
-                        edges.push({from: ctrl.hubs[i].id, to: wifi_node, length: EDGE_LENGTH_MAIN});
-                    }
-                }
 
-                for (var i = 0; i < ctrl.emulators.length; i++) {
-                    nodes.push({
-                        id: ctrl.emulators[i].id,
-                        label: 'Emulator: ' + ctrl.emulators[i].name,
-                        image: DIR + 'Hardware-My-PDA-02-icon.png',
-                        shape: 'image'
-                    });
-
-
-                    //alert(ctrl.emulators[i].attached_hub);
-                    if (ctrl.emulators[i].hub_id != null) {
-
-                        if (ctrl.emulators[i].hub_port)
-
-                            edges.push({
-                                from: ctrl.emulators[i].hub_id,
-                                to: ctrl.emulators[i].id,
-                                label: ctrl.emulators[i].hub_port,
-                                font: {align: 'horizontal'},
-                                length: EDGE_LENGTH_MAIN
+                        //alert(ctrl.hubs[i].network_type);
+                        if (ctrl.hubs[i].network_type == 'WiFi') {
+                            nodes.push({
+                                id: wifi_node,
+                                label: 'WIFI',
+                                image: DIR + 'WIFI.png',
+                                shape: 'image'
                             });
 
-                        else
-
-                            edges.push({
-                                from: ctrl.emulators[i].hub_id,
-                                to: ctrl.emulators[i].id,
-                                length: EDGE_LENGTH_MAIN
-                            });
+                            edges.push({from: ctrl.hubs[i].id, to: wifi_node, length: EDGE_LENGTH_MAIN});
+                        }
                     }
 
+                    for (var i = 0; i < ctrl.emulators.length; i++) {
+                        nodes.push({
+                            id: ctrl.emulators[i].id,
+                            label: 'Emulator: ' + ctrl.emulators[i].name,
+                            image: DIR + 'Hardware-My-PDA-02-icon.png',
+                            shape: 'image'
+                        });
 
-                }
+
+                        //alert(ctrl.emulators[i].attached_hub);
+                        if (ctrl.emulators[i].hub_id != null) {
+
+                            if (ctrl.emulators[i].hub_port)
+
+                                edges.push({
+                                    from: ctrl.emulators[i].hub_id,
+                                    to: ctrl.emulators[i].id,
+                                    label: ctrl.emulators[i].hub_port,
+                                    font: {align: 'horizontal'},
+                                    length: EDGE_LENGTH_MAIN
+                                });
+
+                            else
+
+                                edges.push({
+                                    from: ctrl.emulators[i].hub_id,
+                                    to: ctrl.emulators[i].id,
+                                    length: EDGE_LENGTH_MAIN
+                                });
+                        }
+                    }
+
+                    for (var i = 0; i < ctrl.devices.length; i++) {
+                        nodes.push({
+                            id: ctrl.devices[i].id,
+                            label: 'Device: ' + ctrl.devices[i].name,
+                            image: DIR + 'Hardware-My-PDA-02-icon.png',
+                            shape: 'image'
+                        });
 
 
-                // create a network
-                var container = document.getElementById('mynetwork');
-                var data = {
-                    nodes: nodes,
-                    edges: edges
-                };
-                var options = {};
-                var network = new vis.Network(container, data, options);
+                        //alert(ctrl.emulators[i].attached_hub);
+                        if (ctrl.devices[i].hub_id != null) {
 
-                //alert(ctrl.emulators.length);
-                //alert(ctrl.hubs.length);
+                            if (ctrl.devices[i].hub_port)
 
+                                edges.push({
+                                    from: ctrl.devices[i].hub_id,
+                                    to: ctrl.devices[i].id,
+                                    label: ctrl.devices[i].hub_port,
+                                    font: {align: 'horizontal'},
+                                    length: EDGE_LENGTH_MAIN
+                                });
+
+                            else
+
+                                edges.push({
+                                    from: ctrl.devices[i].hub_id,
+                                    to: ctrl.devices[i].id,
+                                    length: EDGE_LENGTH_MAIN
+                                });
+                        }
+                    }
+
+                    // create a network
+                    var container = document.getElementById('mynetwork');
+                    var data = {
+                        nodes: nodes,
+                        edges: edges
+                    };
+                    var options = {};
+                    var network = new vis.Network(container, data, options);
+
+                    //alert(ctrl.emulators.length);
+                    //alert(ctrl.hubs.length);
+
+                });
             });
         });
-
 
     };
 
     draw();
 
-
-
-    ctrl.devices_num = 0;
 }]);
