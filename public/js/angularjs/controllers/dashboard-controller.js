256f3c14ba06c1f99866d8dc05cfd416fecac047
LipingSun <liping.sun@sjsu.edu>
2015-08-09 03:19:41 -0700

    try fix hub.png

--- public/js/angularjs/controllers/dashboard-controller.js
+++ public/js/angularjs/controllers/dashboard-controller.js
@@ -60,7 +60,7 @@ angular.module('myApp').controller('DashboardController', ['emulatorsService', '
                         nodes.push({
                             id: ctrl.hubs[i].id,
                             label: 'HUB: ' + ctrl.hubs[i].name,
-                            image: DIR + 'HUB.png',
+                            image: DIR + 'hub.png',
                             shape: 'image'
                         });
 
