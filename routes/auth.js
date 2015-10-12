58cc2cf6c3b83a772872c94aad7c0fa3513e7278
LipingSun <liping.sun@sjsu.edu>
2015-10-12 09:58:35 -0700

    add bill plan add duplicated email detection fix register session issue

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var User = require('./../models/User');
+var passport = require('./../services/passport');
 
 var auth = {};
 
@@ -13,16 +14,34 @@ auth.login = function (req, res) {
 };
 
 auth.register = function (req, res) {
-    User.create(req.body, function (err, data) {
+    switch (req.body.bill_plan) {
+        case "Pay AS Hour Go": req.body.bill_plan = "pay_as_hour_go"; break;
+        case "Month Flat Rate": req.body.bill_plan = "month_flat_rate"; break;
+    }
+    var user = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: req.body.password,
+        type: "user",
+        curr_plan: req.body.bill_plan,
+        next_plan: req.body.bill_plan
+    };
+
+    User.create(user, function (err, data) {
         if (err) {
-            res.status(500).send();
+            res.status(500).json("The email is already used, please register with a different email.");
         } else {
-            req.login({email: req.body.email, password: req.body.password}, function(err, next) {
-                if (err) {
-                    return next(err);
-                } else {
-                    return res.status(201).redirect('/');
-                }
+            //req.login({email: req.body.email, password: req.body.password}, function(err, next) {
+            //    if (err) {
+            //        return next(err);
+            //    } else {
+            //        return res.status(201).redirect('/');
+            //    }
+            //});
+
+            passport.authenticate('local')(req, res, function () {
+                res.redirect('/');
             });
         }
     });
@@ -31,7 +50,8 @@ auth.register = function (req, res) {
 
 auth.logout = function (req, res) {
     req.logout();
-    res.json('logout success');
+    res.status(200).redirect('/');
+    //res.json('logout success');
 };
 
 module.exports = auth;
