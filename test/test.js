9457c150af008fbe4a5cab0457ffa4830d111aae
LipingSun <liping.sun@sjsu.edu>
2016-04-22 18:36:02 -0700

    add mock test

---
+++ test/test.js
@@ -0,0 +1,34 @@
+describe('Authentication Module', function() {
+    it('POST /api/v1/login', function () {});
+    it('POST /api/v1/register', function () {});
+    it('DELETE /api/v1/logout', function () {});
+});
+
+describe('User Module', function() {
+    it('GET /api/v1/users', function () {});
+    it('GET /api/v1/users/1', function () {});
+});
+
+describe('Emulator Module', function() {
+    it('POST /api/v1/emulators', function () {});
+    it('POST /api/v1/emulators/1', function () {});
+    it('GET /api/v1/emulators', function () {});
+    it('GET /api/v1/emulators/1', function () {});
+    it('DELETE /api/v1/emulators/1', function () {});
+});
+
+describe('Device Module', function() {
+    it('POST /api/v1/devices', function () {});
+    it('POST /api/v1/devices/1', function () {});
+    it('GET /api/v1/devices', function () {});
+    it('GET /api/v1/devices/1', function () {});
+    it('DELETE /api/v1/devices/1', function () {});
+});
+
+describe('Hub Module', function() {
+    it('POST /api/v1/hubs', function () {});
+    it('POST /api/v1/hubs/1', function () {});
+    it('GET /api/v1/hubs', function () {});
+    it('GET /api/v1/hubs/1', function () {});
+    it('DELETE /api/v1/hubs/1', function () {});
+});
